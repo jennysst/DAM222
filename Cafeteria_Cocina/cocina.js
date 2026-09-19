9050dc25c8f176e25ba00ad8fa866d4a968b8a2b
jennysst <jennyst513800@gmail.com>
2026-09-19 16:32:25 -0600

    Se le agrego ingreso de datos por consola, ademas de que ahora te da a elegir cual es la opcion que se quiere realizar

--- Cafeteria_Cocina/cocina.js
+++ Cafeteria_Cocina/cocina.js
@@ -1,36 +1,74 @@
 
 
 
-const pedidos = ["Espresso", "americano", "cappuccino", "latte", "mocha" ];
+const productos = ["Espresso", "americano", "cappuccino", "latte", "mocha" ];
 
-console.log("------------- Cocina ---------------");
-console.log("Pedido actual");
-console.log("");
-
-console.log(pedidos);
-console.log("");
-
-console.log("AGREGAR Cafe");
-console.log("");
-pedidos.push("Cafe")
-console.log(pedidos);
-console.log("");
-
-console.log("EDITAR Espresso");
-console.log("");
-pedidos[0] = "Té";
-console.log(pedidos);
-console.log("");
-
-console.log("ELIMINAR Té");
-console.log("");
-pedidos.splice(0, 1);
-
-console.log(pedidos);
-console.log("");
-
-console.log("LISTA");
-console.log("");
-pedidos.forEach((pedidos, indice) =>{
-    console.log(indice + 1, " " + pedidos);
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+
+console.log("------------- COCINA ---------------");
+console.log("");
+
+productos.forEach((productos, indice) =>{
+    console.log(indice + 1, " " + productos);
+});
+console.log("");
+console.log("1 - Agregar producto");
+console.log("2 - Editar producto");
+console.log("3 - Eliminar producto");
+console.log("4 - Ver productos");
+console.log("");
+
+rl.question("Elija una opción", (opcion)=>{
+
+    if(opcion == 1){
+        rl.question("¿Que desea agregar?",(p)=>{
+            productos.push(p);
+            productos.forEach((productos, indice) =>{
+            console.log(indice + 1, " " + productos);
+});
+rl.close();
+    });
+    
+    }
+    else
+    if(opcion == 2){
+        rl.question("¿Cual es la posicion del producto a editar?",(po)=>{
+            rl.question("¿Cual es el nuevo producto?",(nuevo)=>{
+            productos[po] = nuevo;
+            productos.forEach((productos, indice) =>{
+            console.log(indice + 1, " " + productos);
+});
+rl.close();
+    });
+    
+    });
+    
+    }
+    else
+    if(opcion == 3){
+        rl.question("¿Cual es la posicion del producto a eliminar?",(el)=>{
+            productos.splice(el, 1);
+            productos.forEach((productos, indice) =>{
+            console.log(indice + 1, " " + productos);
+});
+rl.close();
+    });
+    
+    }
+    else
+    if(opcion == 4){
+        console.log("MENÚ");
+        console.log("");
+        productos.forEach((productos, indice) =>{
+        console.log(indice + 1, " " + productos);
+});
+rl.close();
+    }
+
+});
+
